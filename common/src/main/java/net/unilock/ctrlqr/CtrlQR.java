ade8510a34a32ea79e20214dac653d14e31b966f
unilock <unilock@fennet.rentals>
2023-03-25 09:31:29 -0400

    loosen version requirements, etc.
    Signed-off-by: unilock <unilock@fennet.rentals>

--- common/src/main/java/net/unilock/ctrlqr/CtrlQR.java
+++ common/src/main/java/net/unilock/ctrlqr/CtrlQR.java
@@ -1,11 +1,8 @@
 package net.unilock.ctrlqr;
 
-import java.util.logging.Logger;
-
 public class CtrlQR {
     public static final String MOD_ID = "ctrlqr";
 
     public static void init() {
-        Logger.getLogger(MOD_ID).info("INITIALIZED");
     }
 }
